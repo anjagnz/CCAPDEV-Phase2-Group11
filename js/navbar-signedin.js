adb17b335af0b5237cd465199a41d12192543b73
Liana Ho <143952121+dlpho@users.noreply.github.com>
2025-02-15 01:13:18 +0800

    delete unused files

--- js/navbar-signedin.js
+++
@@ -1,8 +0,0 @@
-fetch('js/navbar-signedin.html')
-.then(response => response.text())
-.then(data => {
-    let oldElement = document.querySelector("script#navigation");
-    let newElement = document.createElement("navbar");
-    newElement.innerHTML = data;
-    oldElement.parentNode.replaceChild(newElement, oldElement);
-})
