60e8d661b109b36268faacfd8eaa1813fe785868
Liana Ho <denise_liana_ho@dlsu.edu.ph>
2025-03-21 11:15:27 +0800

    try part 2

--- index.js
+++ index.js
@@ -873,43 +873,18 @@ hbs.registerHelper('range', function(start, end) {
 });
 
 // // Connect to MongoDB and check for demo profiles
-// mongoose.connect('mongodb://localhost/LabMateDB')
-// .then(() => {
-//     console.log('Connected to MongoDB successfully');
-//     // After successful connection, check and seed the database if needed
-//     checkAndSeedDatabase();
-// })
-// .catch(err => {
-//     console.error('MongoDB connection error:', err);
-// });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://user:user@labmatedb.4fogp.mongodb.net/?retryWrites=true&w=majority&appName=LabMateDB";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+// Connect to MongoDB (if no environtment var, connect to local)
+mongoose.connect(process.env.DATABASE_URL ||'mongodb://localhost/LabMateDB')
+.then(() => {
+    console.log('Connected to MongoDB successfully');
+    // After successful connection, check and seed the database if needed
+    checkAndSeedDatabase();
+})
+.catch(err => {
+    console.error('MongoDB connection error:', err);
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    checkAndSeedDatabase();
-    await client.close();
-    
-  }
-}
-run().catch(console.dir);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
